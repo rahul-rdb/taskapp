57b93b09e646c82140cd40d8a5db5d3c07de2df6
rahul-rdb <rahulbobade26@gmail.com>
2023-08-23 17:04:10 +0530

    router added

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/Layout.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/taskapp/",
+    element: <App />,
+    children: [
+      {
+        path: "/taskapp/",
+        element: <Layout />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/task/",
   plugins: [react()],
-  server: {
-    proxy: {
-      "/task": {
-        target: "/task/index.html",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/task/, ""),
-      },
-    },
-  },
+  base: "/taskapp/",
 });
